018eeb4f721c0d54e77fba0300bc51bd860d9c06
Sachin18022006 <sachinbssachinbs18@gmail.com>
2026-05-03 11:16:50 +0530

    clean auth routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,22 +1,33 @@
 import API from './api.js';
 
+// Register
 const register = async (name, email, password) => {
-  const response = await API.post('/api/auth/register', { name, email, password });
+  const response = await API.post('/auth/register', {
+    name,
+    email,
+    password
+  });
   return response.data.data;
 };
 
+// Login
 const emailLogin = async (email, password) => {
-  const response = await API.post('/api/auth/login', { email, password });
+  const response = await API.post('/auth/login', {
+    email,
+    password
+  });
   return response.data.data;
 };
 
+// Get Profile
 const getMe = async () => {
-  const response = await API.get('/api/auth/me');
+  const response = await API.get('/auth/me');
   return response.data.data;
 };
 
+// Logout
 const logout = async () => {
-  const response = await API.post('/api/auth/logout');
+  const response = await API.post('/auth/logout');
   return response.data;
 };
 
